d70a4af1230d6f33e285dd3c6cbe0e3093419e22
javila35 <josepharthuravila@gmail.com>
2021-04-24 10:30:25 -0700

    Use param for backend Update api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,12 @@
-// const API_ROOT ="http://localhost:3000";
-const API_ROOT = "https://secure-reaches-25083.herokuapp.com";
+const API_ROOT = "http://localhost:3000";
+// const API_ROOT = "https://secure-reaches-25083.herokuapp.com";
 
 export const fetchAllColors = () => {
   return fetch(`${API_ROOT}/all`).then((response) => response.json());
 };
 
 export const fetchColor = (color: string) => {
-  return fetch(`${API_ROOT}/${color}`).then((response) => response.json());
+  return fetch(`${API_ROOT}/colors/${color}`).then((response) =>
+    response.json()
+  );
 };
